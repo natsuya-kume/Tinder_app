c2fa1d27396a812522a1d8cde96a4488d85628ff
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2020-12-28 16:08:18 +0900

    json-server利用

--- tinder_app/src/Components/Card.js
+++ tinder_app/src/Components/Card.js
@@ -1,33 +1,35 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import TinderCard from "react-tinder-card";
-import db from "./userdata.json";
-
-// 既にスワイプしたカードをいれる配列
-const alreadySwiped = [];
-let charactersState = db.users; // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
 
 const Card = () => {
-  // 初期値characters
-  const [characters, setCharacters] = useState(db.users);
-  // 最後にスワイプした方向
-  const [lastDirection, setLastDirection] = useState();
+  const alreadySwiped = []; // スワイプしたカードをいれる配列
 
-  // 引数に作成よう関数とそれが依存する配列を渡す
+  const [characters, setCharacters] = useState([]); // ユーザーデータを管理
+  const [lastDirection, setLastDirection] = useState([]); // スワイプした方向を管理
+  let charactersState = characters; // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
+
+  // json-serverからユーザーデータ取得
+  useEffect(() => {
+    const url = "http://localhost:3000/users";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCharacters(data));
+  }, []);
+
+  // 引数に作成用関数とそれが依存する配列を渡す
   const childRefs = useMemo(
     () =>
-      Array(db.users.length)
+      Array(characters.length)
         .fill(0)
         .map((i) => React.createRef()),
-    []
+    [alreadySwiped]
   );
 
-  // カードがスワイプされる時の関数　引数は方向と消すカードの名前
+  // カードがスワイプされる時の関数　引数はスワイプ方向と消すカードの名前
   const swiped = (direction, nameToDelete) => {
-    console.log(nameToDelete + "が消されました");
-    // 最後にスワイプされたカードを管理;
-    setLastDirection(direction);
-    // 既にスワイプされたカードを管理する配列にスワイプされた名前を代入;
-    alreadySwiped.push(nameToDelete);
+    // console.log(nameToDelete + "が消されました");
+    setLastDirection(direction); // スワイプされた方向を格納;
+    alreadySwiped.push(nameToDelete); // alreadySwipedにスワイプされた名前を代入;
   };
 
   // カードが枠から出た時の関数
@@ -41,7 +43,7 @@ const Card = () => {
     setCharacters(charactersState);
   };
 
-  // スワイプする時(ボタンが押された時)の関数　引数はスワイプスする方向を受け取る
+  // スワイプする時(ボタンが押された時)の関数　引数はスワイプする方向を受け取る
   const swipe = (dir) => {
     console.log(dir + "に移動するボタンを押しました！");
     // スワイプするときに、alreadySwipedに入っていなかった配列を取得
@@ -51,7 +53,9 @@ const Card = () => {
 
     if (cardsLeft.length) {
       const toBeRemoved = cardsLeft[cardsLeft.length - 1].name; // Find the card object to be removed
-      const index = db.users.map((person) => person.name).indexOf(toBeRemoved); // Find the index of which to make the reference to
+      const index = characters
+        .map((person) => person.name)
+        .indexOf(toBeRemoved); // Find the index of which to make the reference to
       alreadySwiped.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
       childRefs[index].current.swipe(dir); // Swipe the card!
     }
@@ -87,7 +91,7 @@ const Card = () => {
           </TinderCard>
         ))}
         {/* 全てのカードが仕分けできた時 */}
-        {alreadySwiped.length >= db.users.length ? (
+        {characters.length === 0 ? (
           <div className="empty">
             <h3>empty!</h3>
           </div>
